7b94014525bd5507372489dbee4130c12ed796b1
Michael Amo <michaelamo@Michaels-MacBook-Pro.local>
2020-06-16 15:50:28 +0000

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        String[] string={"the","my","who"};
+        System.out.println(string[1]);
+
+        Log.d("MainActivity","hello who is this.");
+    }
+}
